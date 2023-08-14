73cb62efed988dde363ae979d198aa276817637e
aluga.com <aluga.com@DSP-HEL>
2023-08-14 07:52:24 -0300

    Add project files.

---
+++ SenaiWebSite.Mvc/Pages/Contato.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SenaiWebSite.Mvc.Pages
+{
+    public class ContatoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SenaiWebSite.Mvc/Pages/Inicial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SenaiWebSite.Mvc.Pages
+{
+    public class InicialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
